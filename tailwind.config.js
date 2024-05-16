c1904357d21ce7731b0f8214e48ef3394a05e44e
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-05-16 23:22:31 +0530

    config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
- 
-module.exports = withMT({
-  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+
+module.exports = ({
+  content: [ "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
+  ],
   theme: {
     extend: {},
   },

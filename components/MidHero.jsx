208901dccbe4e6a1cfc7267c00149d9abfababe7
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-05-16 23:01:37 +0530

    change

--- components/MidHero.jsx
+++ components/MidHero.jsx
@@ -9,7 +9,7 @@ const MidHero = () => {
       </div>
       <div className='col-span-1 flex items-center justify-center flex-col text-white mx-auto text-center'>
         <h1>
-          Calling out all NGO's / Social Welfare Centers 
+          Calling Out all NGO's / Social Welfare Centers 
         </h1>
         <p>Easy Onboarding Process Click Below to Join</p>
         <button className='bg-cyan-300 rounded-xl p-2'>Join</button>

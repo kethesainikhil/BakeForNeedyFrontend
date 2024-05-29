2ffbd588f7468e5b8c4dc32d023698a08928505a
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-05-29 17:10:38 +0530

    fixed dynamic routes

--- pages/donation/[donationId].jsx
+++ pages/donation/[donationId].jsx
@@ -1,27 +1,16 @@
 'use client'
 import React from 'react'
 import DonationIdComponent from '../../components/DonationIdComponent'
-const donationIdPage = ({donationId}) => {
+import { useRouter } from 'next/router'
+const donationIdPage = () => {
 
-    
+  const router = useRouter();
+  const { donationId } = router.query
   return (
     <div>
         <DonationIdComponent donationId={donationId} />
     </div>
   )
 }
-export async function getServerSideProps(context) {
-    const { params } = context;
-    const { donationId } = params; // Get the id from the dynamic route
-  
-    // Fetch data based on the id
-    // Example: const data = await fetchData(id);
-  
-    return {
-      props: {
-        donationId,
-      },
-    };
-  }
 
 export default donationIdPage

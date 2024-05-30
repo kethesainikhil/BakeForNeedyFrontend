2d7d3587966c6a9ac7d1abd5c25292ad955515a5
kethe sai nikhil <nikhilkethe007@gmail.com>
2024-05-30 18:31:09 +0530

    analytics done

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
 import { FloatingNavDemo } from "../components/FloatingDemo";
 import Footer from "../components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 function MyApp({ Component, pageProps }) {
   return (
     
